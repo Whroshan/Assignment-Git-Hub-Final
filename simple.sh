8a102e9c42dec3e5d68ab4cf21154d772c8087e9
Whroshan <113862494+Whroshan@users.noreply.github.com>
2022-12-05 16:26:29 +0100

    Update simple.sh

--- simple.sh
+++ simple.sh
@@ -1,1 +1,9 @@
-Test File for project 
+@@ -6,7 +6,7 @@
+
+   # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # <your GitHub username>
+   # razor50333
+
+   # Input:
+   # p, principal amount
